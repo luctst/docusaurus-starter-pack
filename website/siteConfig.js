4d07188bddce51a4e1a01d442317d0b80021e2ad
luctst <luc.tostee@outlook.fr>
2018-05-31 00:11:47 +0200

    Add the siteConfig.js in sidebar

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -63,7 +63,7 @@ const siteConfig = {
   },*/
 
   // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
-  copyright: 'Copyright © ' + new Date().getFullYear() +' ',
+  copyright: 'Copyright © ' + new Date().getFullYear() +' Tostée Lucas',
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
